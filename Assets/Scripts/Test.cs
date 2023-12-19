fa387a769ce1233791a5da31582384c56290f90d
sergiy <sergiyvaseshev@gmail.com>
2023-12-19 16:40:21 +0200

    add

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,19 +7,35 @@ public class Test : MonoBehaviour
     
     void Start()
     {
-       
+
+        int a = 5;
+        int b = 10;
+        int c = 10;
 
 
+        /*bool result = a > b; //False
+        Debug.Log("  a > b=" + result);
 
-        float floatValue = 10.123f;
-        int intValue = 5;
-        floatValue = intValue;
-      //  intValue = (int )floatValue;
+         result = a < b; //True
+        Debug.Log("  a < b=" + result);
 
-        Debug.Log(" floatValue" + floatValue);
-        //Debug.Log(" intValue" + intValue);
+        result = a ==b; //False
+        Debug.Log("  a == b=" + result);
+
+        result = a != b; //True
+        Debug.Log("  a != b=" + result);*/
+
+        bool result = a>=b; //False
+        Debug.Log("  a >= b=" + result);
+
+         result = b >= c; //True
+        Debug.Log("  b>= c=" + result);
+
+        result = a <= b; //True
+        Debug.Log("  a<= b=" + result);
 
     }
+
 
    
 }
